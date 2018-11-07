aefc3ead1cd828d370c8f7a5f1ea3cdf6b455b4b
JeongHoon, Kim <devkjh@gmail.com>
2018-11-07 15:07:32 +0900

    CSS selected Hero

--- heroes/heroes.component.ts
+++ heroes/heroes.component.ts
@@ -8,6 +8,10 @@ import { HEROES} from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+	selectedHero: Hero;
+	onSelect(hero: Hero): void {
+		this.selectedHero = hero;
+	}
 
 	heroes = HEROES;
 
